2983db5b39324228362bcc73e62317afe199523f
gmega <mega@spaziodati.eu>
2015-07-03 17:36:05 +0200

    fix license reference, add classifiers to setup

--- setup.py
+++ setup.py
@@ -3,15 +3,20 @@ from distutils.core import setup
 setup(
     name='sparmap',
     version='1.0.3',
-    description='Simple parallel map implementation for Python.',
+    description='A Simple PARallel MAP implementation for Python.',
     author='Giuliano Mega',
     author_email='mega@spaziodati.eu',
     py_modules=['sparmap'],
-    license='LICENSE',
+    license='LICENSE.txt',
     install_requires=[
         'multiprocessing >= 2.6.2.1'
     ],
     tests_require=['nose'],
     url=['https://github.com/gmega/sparmap'],
-    download_url='https://github.com/gmega/sparmap/archive/1.0.3.tar.gz'
+    download_url='https://github.com/gmega/sparmap/archive/1.0.3.tar.gz',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License'
+    ]
 )
